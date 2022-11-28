ee3cfbb1e1b6668fc450bf625b484e050150f37c
HossainRafi <hossainrafi71@gmail.com>
2022-11-28 13:10:44 +0600

    cards done

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -7,7 +7,7 @@ const Cards = () => {
   return (
     <div className="w-full py-[10rem] px-4 bg-white">
       <div className="max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8">
-        <div className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
+        <div className="w-full bg-gray-100 shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
           <img
             className="w-20 mx-auto mt-[-3rem] bg-white"
             src={Single}
@@ -20,7 +20,41 @@ const Cards = () => {
             <p className="py-2 border-b mx-8">1 Granted User</p>
             <p className="py-2 border-b mx-8">Send up to 2 GB</p>
           </div>
-          <button className="bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3">
+          <button className="bg-[#00df9a] w-[200px] rounded-md font-bold my-6 mx-auto px-6 py-3">
+            Start Trial
+          </button>
+        </div>
+        <div className="w-full shadow-xl bg-gray-200 flex flex-col p-4 md:my-0 my-8 rounded-lg hover:scale-105 duration-300">
+          <img
+            className="w-20 mx-auto mt-[-3rem] bg-transparent"
+            src={Double}
+            alt="/"
+          />
+          <h2 className="text-2xl font-bold text-center py-8">Two Users</h2>
+          <p className="text-center text-4xl font-bold">$229</p>
+          <div className="text-center font-medium">
+            <p className="py-2 border-b mx-8 mt-8">800 GB Storage</p>
+            <p className="py-2 border-b mx-8">2 Granted User</p>
+            <p className="py-2 border-b mx-8">Send up to 5 GB</p>
+          </div>
+          <button className="bg-black text-[#00df9a] w-[200px] rounded-md font-bold my-6 mx-auto px-6 py-3">
+            Start Trial
+          </button>
+        </div>
+        <div className="w-full bg-gray-100 shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
+          <img
+            className="w-20 mx-auto mt-[-3rem] bg-white"
+            src={Triple}
+            alt="/"
+          />
+          <h2 className="text-2xl font-bold text-center py-8">Three Users</h2>
+          <p className="text-center text-4xl font-bold">$299</p>
+          <div className="text-center font-medium">
+            <p className="py-2 border-b mx-8 mt-8">1000 GB Storage</p>
+            <p className="py-2 border-b mx-8">3 Granted User</p>
+            <p className="py-2 border-b mx-8">Send up to 9 GB</p>
+          </div>
+          <button className="bg-[#00df9a] w-[200px] rounded-md font-bold my-6 mx-auto px-6 py-3">
             Start Trial
           </button>
         </div>
