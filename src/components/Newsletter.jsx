aca30cb5872f0a57959eca53cd036a8625ca7c27
HossainRafi <hossainrafi71@gmail.com>
2022-11-28 12:42:45 +0600

    text done

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Newsletter = () => {
   return (
-    <div>Newsletter</div>
-  )
-}
+    <div className="w-full py-16 text-white px-4">
+      <div className="max-w-[1240px] mx-auto grid lg:grid-cols-3">
+        <div className="lg:col-span-2 my-4">
+          <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold py-2">
+            Want tips & tricks to optimize your flow?
+          </h1>
+          <p>Sign up to our newsletter and stay up to date</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Newsletter
+export default Newsletter;
