4ac25110503f23bda6d50ce37dbcd46b58c9578f
HossainRafi <hossainrafi71@gmail.com>
2022-11-28 13:37:28 +0600

    footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ import {
   FaDribbbleSquare,
   FaFacebookSquare,
   FaGithubSquare,
-  FaInstagram,
   FaTwitterSquare,
+  FaWhatsappSquare,
 } from "react-icons/fa";
 
 const Footer = () => {
@@ -21,7 +21,7 @@ const Footer = () => {
         </p>
         <div className="flex justify-between md:w-[75%] my-6 cursor-pointer">
           <FaFacebookSquare size={30} />
-          <FaInstagram size={30} />
+          <FaWhatsappSquare size={30} />
           <FaTwitterSquare size={30} />
           <FaGithubSquare size={30} />
           <FaDribbbleSquare size={30} />
@@ -47,7 +47,24 @@ const Footer = () => {
             <li className="py-2 text-sm">API Status</li>
           </ul>
         </div>
-        
+        <div>
+          <h6 className="font-medium text-gray-400">Company</h6>
+          <ul>
+            <li className="py-2 text-sm">About</li>
+            <li className="py-2 text-sm">Blog</li>
+            <li className="py-2 text-sm">Jobs</li>
+            <li className="py-2 text-sm">Press</li>
+            <li className="py-2 text-sm">Careers</li>
+          </ul>
+        </div>
+        <div>
+          <h6 className="font-medium text-gray-400">Legal</h6>
+          <ul>
+            <li className="py-2 text-sm">Claim</li>
+            <li className="py-2 text-sm">Policy</li>
+            <li className="py-2 text-sm">Terms</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
