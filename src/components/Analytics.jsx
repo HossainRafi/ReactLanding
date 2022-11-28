49a056516dd4195f8a922476244057f5a833a62e
HossainRafi <hossainrafi71@gmail.com>
2022-11-28 12:32:01 +0600

    img added

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import Laptop from "../assets/laptop.jpg";
 
 const Analytics = () => {
   return (
-    <div>Analytics</div>
-  )
-}
+    <div className="w-full bg-white py-16 px-4">
+      <div className="max-w-[1240px] mx-auto">
+        <img className="w-[500px] mx-auto my-4" src={Laptop} alt="/" />
+        
+      </div>
+    </div>
+  );
+};
 
-export default Analytics
+export default Analytics;
