bfb632392e80fa98ee167dd7662c0ab9146924dd
HossainRafi <hossainrafi71@gmail.com>
2022-11-28 12:54:29 +0600

    btn done

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -15,9 +15,16 @@ const Newsletter = () => {
             <input
               className="p-3 flex w-full rounded-md text-black outline-none"
               type="email"
-              placeholder="Enter Your E-Mail Address"
+              placeholder="Enter Your E-Mail"
             />
+            <button className="bg-[#00df9a] text-black rounded-md font-medium w-[200px] ml-4 my-6 px-4 py-3">
+              Notify Me
+            </button>
           </div>
+          <p>
+            We care bout the protection of your data. Read our{" "}
+            <span className="text-[#00df9a]">Privacy Policy.</span>
+          </p>
         </div>
       </div>
     </div>
