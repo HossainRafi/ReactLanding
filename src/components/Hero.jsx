ed061ac96098e2d2ed8b9ae542781b3776b20b28
HossainRafi <hossainrafi71@gmail.com>
2022-11-28 12:06:31 +0600

    title

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div className="text-white">
+      <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
+        <p className="text-[#00df9a] font-bold text-xl md:text-2xl p-2">
+          GROWING WITH DATA ANALYTICS
+        </p>
+        <h1 className="md:text-6xl sm:text-5xl text-4xl font-bold md:py-6">
+          Grow with data
+        </h1>
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
