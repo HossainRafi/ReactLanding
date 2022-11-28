52085c55af1a3563cb54b045f24b4337a923b05f
HossainRafi <hossainrafi71@gmail.com>
2022-11-28 11:31:54 +0600

    nav logo and routes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-2 text-white">
+      <h1 className="w-full text-3xl font-bold text-[#00df9a]">Landing UI</h1>
+      <ul className="flex">
+        <li className="p-4">Home</li>
+        <li className="p-4">Company</li>
+        <li className="p-4">Resources</li>
+        <li className="p-4">About</li>
+        <li className="p-4">Contact</li>
+      </ul>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
