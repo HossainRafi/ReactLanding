0340fb95a45fff0d1533526e5ef7bcbf5129e2e9
HossainRafi <hossainrafi71@gmail.com>
2022-11-28 12:10:17 +0600

    text done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,6 +10,18 @@ const Hero = () => {
         <h1 className="md:text-6xl sm:text-5xl text-4xl font-bold md:py-6">
           Grow with data
         </h1>
+        <div className="flex justify-center items-center">
+          <p className="md:text-5xl sm:text-4xl text-xl font-bold py-4">
+            Fast, flexible financing for
+          </p>
+        </div>
+        <p className="md:text-2xl text-xl font-bold text-gray-500">
+          Monitor your data analytics to increase revenue for BTB, BTC, & SASS
+          platforms.
+        </p>
+        <button className="bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black">
+          Get Started
+        </button>
       </div>
     </div>
   );
