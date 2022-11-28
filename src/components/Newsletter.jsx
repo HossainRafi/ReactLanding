e50ad14141e4114044194ba07929bfe13cdfc5f7
HossainRafi <hossainrafi71@gmail.com>
2022-11-28 12:51:56 +0600

    input done

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -10,6 +10,15 @@ const Newsletter = () => {
           </h1>
           <p>Sign up to our newsletter and stay up to date</p>
         </div>
+        <div className="my-4">
+          <div className="flex flex-col sm:flex-row items-center justify-between w-full">
+            <input
+              className="p-3 flex w-full rounded-md text-black outline-none"
+              type="email"
+              placeholder="Enter Your E-Mail Address"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
